20a91fa5aa999088d3de7511fcdb54fdbda64e97
MtnDont <csbartlow99@gmail.com>
2025-12-15 21:20:00 -0600

    Add item graphic to RouteExplorationScreen

--- app/src/main/java/com/mtndont/smartpokewalker/presentation/RouteExplorationScreen.kt
+++ app/src/main/java/com/mtndont/smartpokewalker/presentation/RouteExplorationScreen.kt
@@ -44,9 +44,11 @@ import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,7 +58,9 @@ import androidx.wear.compose.foundation.CurvedLayout
 import androidx.wear.compose.foundation.CurvedModifier
 import androidx.wear.compose.foundation.curvedRow
 import androidx.wear.compose.material3.CircularProgressIndicator
+import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.ProgressIndicatorDefaults
+import androidx.wear.compose.material3.Text
 import androidx.wear.compose.material3.curvedText
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.mtndont.smartpokewalker.R
@@ -98,6 +102,7 @@ fun RouteExplorationScreen(
     }
 }
 
+@SuppressLint("ResourceType")
 @Composable
 fun FoundMonsterAnimation(
     modifier: Modifier = Modifier,
@@ -150,14 +155,35 @@ fun FoundMonsterAnimation(
 
         MonsterImage(
             monsterResId = newMonster.getFormResId(),
-            name = stringResource(
-                R.string.name_and_item,
-                newMonster.getDefaultName(),
-                newItem.name
-            ),
+            name = newMonster.getDefaultName(),
             modifier = Modifier
                 .fillMaxSize()
-        )
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = BitmapPainter(
+                        image = ImageBitmap.imageResource(R.raw.item),
+                        filterQuality = FilterQuality.None
+                    ),
+                    contentDescription = null,
+                    tint = colorResource(R.color.black),
+                    modifier = Modifier
+                        .size(18.dp)
+                        .padding(end = 4.dp)
+                )
+                Text(
+                    text = newItem.name,
+                    color = colorResource(R.color.black),
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily(
+                        Font(R.font.pixelfont)
+                    )
+                )
+            }
+        }
 
         CurvedLayout(
             anchor = 270f + 360f * edgeTextRotate,
